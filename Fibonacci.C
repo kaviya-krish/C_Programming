6ecfe90dbce498cc67b40de622b98118f4716c89
KAVIYA N <166895442+kaviya-krish@users.noreply.github.com>
2026-02-10 19:21:02 +0530

    Create Fibonacci.C

---
+++ Fibonacci.C
@@ -0,0 +1,25 @@
+#include <stdio.h>
+void fibonacci(int n)
+{
+    if(n==0)
+    return 0;
+    if(n==1)
+    return 1;
+    
+    int a=0,b=1;
+    for(int i=2;i<=n;i++)
+    {
+        int c=a+b;
+        a=b;
+        b=c;
+    }
+    printf("%d",b);
+}
+
+int main() {
+	int n;
+	scanf("%d",&n);
+	fibonacci(n);
+
+}
+
